e18cdf91269ac7f03ced4c8e2ac274e593fab05f
qewrt <alikai812@qq.com>
2019-10-24 10:47:49 +0800

    [*]目标测试二进制

---
+++ app/src/main/cpp/bintarget/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int count = 0;
+
+void sevenWeapons(int number)
+{
+    char* str = "Hello,LiBieGou!";
+    printf("%s %d\n",str,number);
+}
+
+int main()
+{
+    while(1)
+    {
+        sevenWeapons(count);
+        count++;
+        sleep(1);
+    }
+    return 0;
+}
